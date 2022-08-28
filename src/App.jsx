912a412288145332a79ced208ca7de5c675a7e6a
StevenCM199 <stevencm199@gmail.com>
2022-08-28 15:49:14 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import "./styles.css";
+import { useState } from "react";
+import colors from "./colors.json";
+import quotes from './quotes.json';
+
+function App() {
+
+  var colorArray = Object.values(colors); 
+
+
+  let randomIndex = Math.floor(Math.random() * quotes.length);
+  let randomColor = Math.floor(Math.random() * colorArray.length);
+
+  const [index, setIndex] = useState(randomIndex); 
+  const [color, setColor] = useState(randomColor); 
+
+
+  document.body.style = `background: ${colorArray[randomColor]}`;
+  
+
+  function changeIndex() {
+    setIndex(randomIndex);
+    setColor(randomColor);
+    console.log(randomIndex); 
+  }
+
+
+  return (
+    <div className="App">
+      <div className="card" style={{color: colorArray[randomColor]}}>
+        <div className="quote-content">
+          <h1 className="quotation-mark"> <i class="fa-solid fa-quote-left"></i>
+          </h1>
+          <p className="quote-text">
+            {quotes[randomIndex].quote}
+          </p>
+        </div>
+        
+        <p className="author-text">
+          {'-'}{quotes[randomIndex].author}
+        </p>
+
+        <div className="button-container">
+          <button onClick={changeIndex}> New Quote</button>
+
+        </div>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
+
